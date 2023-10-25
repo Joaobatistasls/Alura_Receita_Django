480858890c69facf78eb073ecc36b6707265cdc5
João Batista <joaobatistaoficial44@gmail.com>
2023-10-25 07:44:48 -0300

    Usando models para minha aplicação

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'alurareceita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alura_receita',
+        'USER': 'postgres',
+        'PASSWORD': '123456', 
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
--- receita/models.py
+++ receita/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Receita(models.Model):
+    # Criando os campos da minha receita.
+    # Criando o nome da receita - CharField - Para texto
+    nome_receita = models.CharField(max_length=200)
+    # Ingredientes - TextField - Para texto maior  
+    ingredientes = models.TextField()
+    # Modo de Preparo - TextField - Para texto maior 
+    modo_preparo = models.TextField()
+    # Tempo de Preparo - IntegerField - Para numero inteiro
+    tempo_preparo = models.IntegerField()
+    # Rendimento - CharField - Para texto
+    redimento = models.CharField(max_length=100)
+    # Categoria - CharField - Para texto
+    categoria = models.CharField(max_length=100)
+    # Data da Receita - Datetime - Para Data e Hora
+    date_receita = models.DateTimeField(default=datetime.now, blank=True)
+    
